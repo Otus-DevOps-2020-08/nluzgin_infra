8271dba405c849944b5e1ccb21a2be3fbad9ae92
nluzgin <nluzgin@mail.ru>
2020-06-25 20:45:58 +0300

    F**ing Travis fix?

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ sudo apt install -y ruby-full ruby-bundler build-essential
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D68FA50FEA312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo bash -c 'echo "deb [trusted=yes] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl enable mongod
