2616bf9ada7e026ddab9d4162efda40825a21358
nluzgin <nluzgin@mail.ru>
2020-07-09 15:16:43 +0300

    terraform DZ

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,27 +5,25 @@ terraform {
 
 provider "google" {
   # Версия провайдера
-  version = "2.15"
-
-  # ID проекта
-  project = "infra-280913"
-
-  region = "europe-west-1"
+  version = "2.15.0"
+  project = var.project
+  region  = var.region
 }
 
 resource "google_compute_instance" "app" {
-  name = "reddit-app-by-terraform"
+  name         = "reddit-app-by-terraform"
   machine_type = "g1-small"
-  zone = "europe-west1-b"
-  tags = ["reddit-app"]
+  zone         = var.zone
+  tags         = ["reddit-app"]
+
   boot_disk {
     initialize_params {
-      image = "reddit-full-1593289205"
+      image = var.disk_image
     }
   }
   metadata = {
     # путь до публичного ключа
-    ssh-keys = "nluzgin:${file("~/.ssh/id_rsa.pub")}"
+    ssh-keys = "nluzgin:${file(var.public_key_path)}"
   }
 
   network_interface {
@@ -34,22 +32,40 @@ resource "google_compute_instance" "app" {
   }
 
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "/home/user/Documents/OTUS/nluzgin_infra/terraform/files/puma.service"
     destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+
+  connection {
+    type  = "ssh"
+    host  = self.network_interface[0].access_config[0].nat_ip
+    user  = "nluzgin"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
   }
 }
 
 resource "google_compute_firewall" "firewall_puma" {
   name = "allow-puma-default"
+
   # Название сети, в которой действует правило
+
   network = "default"
+
   # Какой доступ разрешить
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
+
   # Каким адресам разрешаем доступ
   source_ranges = ["0.0.0.0/0"]
+
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
 }
